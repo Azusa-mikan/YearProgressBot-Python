ec49f7f99f6293d673abb014822dc10dd369eab3
azusa-mikan <diao0cheng@gmail.com>
2025-10-30 14:56:00 +0800

    可运行

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 import datetime
 import pytz
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 load_dotenv()
 
@@ -23,7 +26,7 @@ def is_leap_year(year: int) -> bool:
     else:
         return True
 
-def get_year_progress():
+def get_year_progress() -> float:
     tz = os.getenv("TZ")
     if tz is None:
         current_date = datetime.datetime.now()
@@ -36,10 +39,14 @@ def get_year_progress():
         days_in_year = 366
     else:
         days_in_year = 365
-    progress = int((day_of_year / days_in_year) * 100)
+    progress = (day_of_year / days_in_year) * 100
     return progress
 
-
+def generate_progress_bar(percentage, length=20):
+    filled = int(length * percentage / 100)
+    blank = length - filled
+    bar = "▓" * filled + "░" * blank
+    return f"{bar} {int(percentage)}%"
 
 def send_message(msg: str):
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
@@ -48,3 +55,14 @@ def send_message(msg: str):
     chat_id_list = chat_id.split(",")
     for id in chat_id_list:
         bot.send_message(id, msg)
+
+@bot.message_handler(commands=["get"])
+def progress(message):
+    progress = get_year_progress()
+    progress_bar = generate_progress_bar(progress)
+    bot.send_message(message.chat.id, f"{progress_bar}")
+
+if __name__ == "__main__":
+    logging.info("YearProgressBot is running")
+    bot.infinity_polling()
+
